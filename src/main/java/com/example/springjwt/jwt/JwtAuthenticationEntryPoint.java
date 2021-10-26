2c7c4bbad62091c7cebf3d5e7529e83507df9e71
tmdrl5779 <tmdrl5779@gmail.com>
2021-10-26 19:32:38 +0900

    [ADD] JwtSecurityConfig - TokenProvider, JwtFilter를 SecurityConfig에 적용 [ADD] JwtAuthenticationEntryPoint - 유효한 자격증명을 제공하지 않고 접근할때 401 에러 반환 [ADD] JwtAccessDeniedHandler - 필요한 권한이 없이 접근하려 할때 403 반환

---
+++ src/main/java/com/example/springjwt/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,21 @@
+package com.example.springjwt.jwt;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//유효한 자격증명을 제공하지 않고 접근할때 401 에러 반환
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
---
+++ src/main/java/com/example/springjwt/jwt/JwtSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.springjwt.jwt;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> { //TokenProvider, JwtFilter를 SecurityConfig에 적용할떄 사용
+    private TokenProvider tokenProvider;
+
+    public JwtSecurityConfig(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) {
+        JwtFilter customFilter = new JwtFilter(tokenProvider);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
