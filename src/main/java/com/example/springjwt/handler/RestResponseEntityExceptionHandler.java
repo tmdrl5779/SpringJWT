e040676df5daa71a180387c59192ac47500dadc7
tmdrl5779 <tmdrl5779@gmail.com>
2021-11-02 02:21:03 +0900

    [ADD]  중복 회원가입 관련 예외처리

--- src/main/java/com/example/springjwt/handler/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/example/springjwt/handler/RestResponseEntityExceptionHandler.java
@@ -1,2 +1,23 @@
-package com.example.springjwt.handler;public class RestResponseEntityExceptionHandler {
+package com.example.springjwt.handler;
+
+import static org.springframework.http.HttpStatus.CONFLICT;
+
+import com.example.springjwt.dto.ErrorDto;
+import com.example.springjwt.exception.DuplicateMemberException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ResponseStatus(CONFLICT)
+    @ExceptionHandler(value = { DuplicateMemberException.class })
+    @ResponseBody
+    protected ErrorDto badRequest(RuntimeException ex, WebRequest request) {
+        return new ErrorDto(CONFLICT.value(), ex.getMessage());
+    }
 }
