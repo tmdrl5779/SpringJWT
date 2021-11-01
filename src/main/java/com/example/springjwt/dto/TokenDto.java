6b3fcbb0c71c675c77aa8bd68929b092a2f86ce3
tmdrl5779 <tmdrl5779@gmail.com>
2021-11-02 00:55:57 +0900

    [ADD] LoginDto, TokenDto, UserDto

---
+++ src/main/java/com/example/springjwt/dto/TokenDto.java
@@ -0,0 +1,14 @@
+package com.example.springjwt.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String token;
+
+}
